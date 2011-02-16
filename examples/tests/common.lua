4d621c8e4c67a8f28a7c73de586d44f8d2df4c8e
stevedonovan <steve.j.donovan@gmail.com>
2011-02-16 18:15:08 +0200

    test fails in a more useful way

--- examples/tests/common.lua
+++ examples/tests/common.lua
@@ -5,5 +5,11 @@ function dump(t)
 end
 
 function assert_eq(doc,str)
-    assert(h.tostring(doc) == str)
+    local s = h.tostring(doc)
+    if s ~= str then
+        print('h',s)
+        print('s',str)
+        io.stderr:write('were not equal\n')
+        os.exit(1)
+    end
 end
